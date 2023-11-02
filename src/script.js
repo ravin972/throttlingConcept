26698e387a08c61d536cda3ff44172a65c0a0648
Ravinder Pandey <59820924+ravin972@users.noreply.github.com>
2023-11-02 11:22:37 +0530

    js update

--- src/script.js
+++ src/script.js
@@ -13,21 +13,27 @@ const throttleFunction = (func, delay) => {
 
 document.querySelector(".container").addEventListener(
   "mousemove",
-  throttleFunction(() => {
+  throttleFunction((e) => {
     // Create a new <div> element
     var customDiv = document.createElement("div");
 
     // Set attributes and properties
     customDiv.id = "myCustomDiv";
     customDiv.className = "custom-div";
+    customDiv.style.top = e.clientY + "px";
+    customDiv.style.left = e.clientX + "px";
 
     // Set CSS styles (height, width, position, and background color)
     customDiv.style.height = "150px"; // Set the height to 100 pixels
     customDiv.style.width = "120px"; // Set the width to 200 pixels
     customDiv.style.position = "absolute"; // Set the position to absolute
+    customDiv.style.overflow  = "hidden"; // Set the overflow to hidden
     customDiv.style.backgroundColor = "lightblue"; // Set the background color to light blue
 
     // Append it to the body or another container
     document.body.appendChild(customDiv);
-  }, 500)
+    setTimeout(function() {
+      customDiv.remove();
+    }, 1000);
+  }, 300)
 );
