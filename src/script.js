b5adecfe13b6f4bd540ee32fc99ba34b670684f0
Ravinder Pandey <59820924+ravin972@users.noreply.github.com>
2023-11-02 12:36:57 +0530

    new update//complete

--- src/script.js
+++ src/script.js
@@ -27,12 +27,57 @@ document.querySelector(".container").addEventListener(
     customDiv.style.height = "150px"; // Set the height to 100 pixels
     customDiv.style.width = "120px"; // Set the width to 200 pixels
     customDiv.style.position = "absolute"; // Set the position to absolute
-    customDiv.style.overflow  = "hidden"; // Set the overflow to hidden
-    customDiv.style.backgroundColor = "lightblue"; // Set the background color to light blue
+    customDiv.style.overflow = "hidden"; // Set the overflow to hidden
+
+    // var customImg = document.createElement("img");
+    // customImg.setAttribute("src", "");
+    // customImg.style.height = "100%"; // Set the height
+    // customImg.style.width = "100%"; // Set the width
+    // customImg.style.objectFit = "cover"; // Set the object fit to cover
+    // // Append the image to the customDiv
+    // customDiv.appendChild(customImg);
+
+    // // Append the customDiv to the container with the class "container"
+    // document.querySelector(".container").appendChild(customDiv);
+
+    // Create an array of image URLs
+    const imageUrls = [
+      "https://c4.wallpaperflare.com/wallpaper/507/287/721/jujutsu-kaisen-gojo-satoru-satoru-gojo-anime-city-hd-wallpaper-preview.jpg",
+      "https://wallpaperaccess.com/full/8287917.jpg",
+      "https://wallpaperaccess.com/full/7797332.jpg",
+    ];
+
+    // Create and append multiple images to the customDiv
+    for (const imageUrl of imageUrls) {
+      var customImg = document.createElement("img");
+      customImg.setAttribute("src", imageUrl);
+      customImg.style.height = "100%"; // Set the height
+      customImg.style.width = "100%"; // Set the width
+      customImg.style.objectFit = "cover"; // Set the object fit to cover
+      customImg.style.transform = "translateY(100%)";
+      customDiv.appendChild(customImg);
+    }
+
+    // Append the customDiv to the container with the class "container"
+    document.querySelector(".container").appendChild(customDiv);
+
+    // Animation with GSAP
+    gsap.to(customDiv.querySelectorAll("img"), {
+      y: 0,
+      ease: "power1.inOut", // Specify the easing as a string
+      duration: 0.2,
+    });
+
+    gsap.to(customDiv.querySelectorAll("img"), {
+      y: "100%",
+      delay: 0.3,
+      ease: "power2.inOut", // Specify the easing as a string
+      duration: 0.3,
+    });
 
     // Append it to the body or another container
     document.body.appendChild(customDiv);
-    setTimeout(function() {
+    setTimeout(function () {
       customDiv.remove();
     }, 1000);
   }, 300)
